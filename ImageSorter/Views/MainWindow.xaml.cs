002e0ed8955f2c4f15f653d203cf17605f4ac597
Daniel Kuglitsch <Daniel.Kuglitsch@gmail.com>
2018-02-04 22:07:21 +0100

    small changes

--- ImageSorter/Views/MainWindow.xaml.cs
+++ ImageSorter/Views/MainWindow.xaml.cs
@@ -1,13 +1,13 @@
 ﻿using ImageSorter.Properties;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using static ImageSorter.Helpers.CompareHelper;
 
 namespace ImageSorter
 {
@@ -19,6 +19,7 @@ namespace ImageSorter
         private List<FileInfo> filePaths;
         private int selectedIndex = 0;
         private bool isMenuHidden = false;
+        private string initialPath = null;
 
         public MainWindow()
         {
@@ -27,137 +28,12 @@ namespace ImageSorter
 
         #region Helpers
 
-        #region SorterWindowsStyle
-
-        public static int CompareNatural(string strA, string strB)
-        {
-            return CompareNatural(strA, strB, CultureInfo.CurrentCulture, CompareOptions.IgnoreCase);
-        }
-
-        public static int CompareNatural(string strA, string strB, CultureInfo culture, CompareOptions options)
-        {
-            CompareInfo cmp = culture.CompareInfo;
-            int iA = 0;
-            int iB = 0;
-            int softResult = 0;
-            int softResultWeight = 0;
-            while (iA < strA.Length && iB < strB.Length)
-            {
-                bool isDigitA = Char.IsDigit(strA[iA]);
-                bool isDigitB = Char.IsDigit(strB[iB]);
-                if (isDigitA != isDigitB)
-                {
-                    return cmp.Compare(strA, iA, strB, iB, options);
-                }
-                else if (!isDigitA && !isDigitB)
-                {
-                    int jA = iA + 1;
-                    int jB = iB + 1;
-                    while (jA < strA.Length && !Char.IsDigit(strA[jA])) jA++;
-                    while (jB < strB.Length && !Char.IsDigit(strB[jB])) jB++;
-                    int cmpResult = cmp.Compare(strA, iA, jA - iA, strB, iB, jB - iB, options);
-                    if (cmpResult != 0)
-                    {
-                        string sectionA = strA.Substring(iA, jA - iA);
-                        string sectionB = strB.Substring(iB, jB - iB);
-                        if (cmp.Compare(sectionA + "1", sectionB + "2", options) ==
-                            cmp.Compare(sectionA + "2", sectionB + "1", options))
-                        {
-                            return cmp.Compare(strA, iA, strB, iB, options);
-                        }
-                        else if (softResultWeight < 1)
-                        {
-                            softResult = cmpResult;
-                            softResultWeight = 1;
-                        }
-                    }
-                    iA = jA;
-                    iB = jB;
-                }
-                else
-                {
-                    char zeroA = (char)(strA[iA] - (int)Char.GetNumericValue(strA[iA]));
-                    char zeroB = (char)(strB[iB] - (int)Char.GetNumericValue(strB[iB]));
-                    int jA = iA;
-                    int jB = iB;
-                    while (jA < strA.Length && strA[jA] == zeroA) jA++;
-                    while (jB < strB.Length && strB[jB] == zeroB) jB++;
-                    int resultIfSameLength = 0;
-                    do
-                    {
-                        isDigitA = jA < strA.Length && Char.IsDigit(strA[jA]);
-                        isDigitB = jB < strB.Length && Char.IsDigit(strB[jB]);
-                        int numA = isDigitA ? (int)Char.GetNumericValue(strA[jA]) : 0;
-                        int numB = isDigitB ? (int)Char.GetNumericValue(strB[jB]) : 0;
-                        if (isDigitA && (char)(strA[jA] - numA) != zeroA) isDigitA = false;
-                        if (isDigitB && (char)(strB[jB] - numB) != zeroB) isDigitB = false;
-                        if (isDigitA && isDigitB)
-                        {
-                            if (numA != numB && resultIfSameLength == 0)
-                            {
-                                resultIfSameLength = numA < numB ? -1 : 1;
-                            }
-                            jA++;
-                            jB++;
-                        }
-                    }
-                    while (isDigitA && isDigitB);
-                    if (isDigitA != isDigitB)
-                    {
-                        return isDigitA ? 1 : -1;
-                    }
-                    else if (resultIfSameLength != 0)
-                    {
-                        return resultIfSameLength;
-                    }
-                    int lA = jA - iA;
-                    int lB = jB - iB;
-                    if (lA != lB)
-                    {
-                        return lA > lB ? -1 : 1;
-                    }
-                    else if (zeroA != zeroB && softResultWeight < 2)
-                    {
-                        softResult = cmp.Compare(strA, iA, 1, strB, iB, 1, options);
-                        softResultWeight = 2;
-                    }
-                    iA = jA;
-                    iB = jB;
-                }
-            }
-            if (iA < strA.Length || iB < strB.Length)
-            {
-                return iA < strA.Length ? 1 : -1;
-            }
-            else if (softResult != 0)
-            {
-                return softResult;
-            }
-            return 0;
-        }
-
-        public class CustomComparer<T> : IComparer<T>
-        {
-            private Comparison<T> _comparison;
-
-            public CustomComparer(Comparison<T> comparison)
-            {
-                _comparison = comparison;
-            }
-
-            public int Compare(T x, T y)
-            {
-                return _comparison(x, y);
-            }
-        }
-
-        #endregion SorterWindowsStyle
 
         private void ChangeSelection()
         {
             snackNotify.IsActive = false;
             imageHost.Source = new BitmapImage(new Uri(filePaths.ElementAt(selectedIndex).FullName));
-            this.Title = filePaths.ElementAt(selectedIndex).Name + $"({selectedIndex + 1}/{filePaths.Count})";
+            this.Title = $"({selectedIndex + 1}/{filePaths.Count}) - {filePaths.ElementAt(selectedIndex)?.Name ?? "err"}";
         }
 
         private void SaveCurrentPicture(bool isUpArrow)
@@ -226,6 +102,16 @@ namespace ImageSorter
 
         private void LoadItemsFromPath()
         {
+            if(initialPath == null)
+            {
+                initialPath = Settings.Default.LoadPath;
+                selectedIndex = 0;
+            }
+            else if(initialPath != Settings.Default.LoadPath)
+            {
+                selectedIndex = 0; 
+            }
+
             if (string.IsNullOrEmpty(Settings.Default.LoadPath))
             {
                 this.Close();
